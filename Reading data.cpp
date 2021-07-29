ecbf02e3eae99c689f30075acfff545b6d88ed62
Ivan(Janis) <85873435+janisivanoov@users.noreply.github.com>
2021-07-29 15:35:30 +0300

    Add files via upload

---
+++ Reading data.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+int main() {
+	system("chcp 1251 > nul");
+	char ch;
+	int j;
+	double d;
+	string str1;
+	string str2;
+	ifstream infile("text.txt");
+	infile >> ch >> j >> d >> str1 >> str2;
+	cout << ch << j << d << str1 << str2 << endl;
+	system("pause > 0");
+	return 0;
+}
