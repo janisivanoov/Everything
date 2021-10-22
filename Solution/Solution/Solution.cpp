e511d378d7b5e8f4d6253469eecfc3b4c439b893
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-10-22 22:12:13 +0300

    Add files via upload

---
+++ Solution/Solution/Solution.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	cout << "SCORE\t" << "GRADE" << endl;
+	cout << "100\t" << "A\n";
+	cout << "80\t" << "B\n";
+	cout << "70\t" << "C\n";
+	cout << "60\t" << "D\n";
+
+	cout << "	" << endl;
+
+	cout << "SCORE\t" << "GRADE\n" << "100\t" << "A\n""80\t" << "B\n" << "70\t" << "C\n" << "60\t" << "D\n";
+
+	//Ending the code
+	return 0;
+}
