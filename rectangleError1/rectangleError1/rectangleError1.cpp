3f5374944d6133ce0c2c4583dd4e8e54782d3477
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-01-05 21:35:02 +0200

    Add files via upload

---
+++ rectangleError1/rectangleError1/rectangleError1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double h, w
+
+		cout << "Enter a width\n";
+	cin >> w;
+
+	cout << "Enter a height\n";
+	cin >> h;
+
+	cout << "Rectangle width: " << w << endl;
+	cout << "Rectangle height: " << h << endl;
+
+	cout << "The perimeter of the rectangle is: " << 2 * (h + w) << endl;
+	cout << "The area of the rectangle is: " << h * w << endl;
+
+	return 0;
+}
